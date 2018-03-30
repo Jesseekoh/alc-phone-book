d9977dc330f36b283369594d844b2ef0bb67f32a
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2018-03-30 09:51:21 +0100

    added a modal for the add new contact form

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,28 @@
 //declare variable
-var contactList = [{
-  name: 'jesse',
-  phone: '090987',
-  mobile: '090988',
-  address: 'schjd',
-  email: 'dalj',
+var contactList = [
+  {
+    name: 'jesse',
+    phone: '090987',
+    mobile: '090988',
+    address: 'schjd',
+    email: 'dalj',
 
-},{
-  name: 'john',
-  phone: 'fj',
-  mobile: 'fcg',
-  address: '',
-  email: '',
+  },{
+    name: 'john',
+    phone: 'fj',
+    mobile: 'fcg',
+    address: '',
+    email: '',
 
-},{
-  name: 'jane',
-  phone: 'f',
-  mobile: 'fdf',
-  address: '',
-  email: '',
+  },{
+    name: 'jane',
+    phone: 'f',
+    mobile: 'fdf',
+    address: '',
+    email: '',
 
-}];
+  }
+];
 
 var contact = {
   name: '',
@@ -32,11 +34,40 @@ var contact = {
 
 var list = $('#contact-list');
 
-//loops through the contactList and appends each contact name to a ul
-for(var i=0;i<contactList.length;i++){
-  //create a list item with the contact name 
- var item = '<li>' + contactList[i].name + '</li>';
- //appends the li to list
- list.append($(item));
+function showContacts(){
+  //loops through the contactList and appends each contact name to a ul
+  for(var i=0;i<contactList.length;i++){
+    //create a list  item with the contact name 
+   var item = '<div class="info">' + '<li>' + contactList[i].name + '</li>' + '<span class="test"></span>' + '</div>';
+   //appends the li to list
+   list.append($(item));
 
+  }
+  
 }
+showContacts();
+
+var saveButton = $('#submit')
+console.log(saveButton);
+
+saveButton.on('click',function(){
+  console.log('clicked')
+  contact.name = $('#name').val();
+  contact.phone = $('#phone').val();
+  contact.mobile = $('#mobile').val();
+  contact.address = $('#address').val();
+  contact.email = $('#email').val();
+  console.log(contact)
+  console.log(contactList);
+  contactList.push(contact);
+  console.log('new list is: ',contactList)
+  list.empty();
+  $('#name').val('');
+  $('#phone').val('');
+  $('#mobile').val('');
+  $('#address').val('');
+  $('#email').val('');
+  showContacts();
+})
+
+
